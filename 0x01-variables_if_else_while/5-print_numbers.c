833e01ae0d8d328c68f0d2b8da8fff812ad0994b
Paul <bizimapaul777@gmail.com>
2023-07-15 07:44:18 -0700

    hh

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+    int i;
+
+    /* Loop through the single-digit numbers */
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d", i);
+    }
+
+    printf("\n");
+
+    return (0);
+}

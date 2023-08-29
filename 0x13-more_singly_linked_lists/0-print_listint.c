7a10f158b15cd16e80f16f20e14aa17812947196
Paul <bizimspaul2017@gmail.com>
2023-08-29 10:11:13 -0700

    changing betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 	const listint_t *current = h;
 
-	while (current!= NULL)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;

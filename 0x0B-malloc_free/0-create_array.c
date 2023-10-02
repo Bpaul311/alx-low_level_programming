369abe7b66fbc8d4fa11bbb6627988afb43c090f
Bpaul311 <bizimapaul777@gmail.com>
2023-10-02 10:28:16 -0700

    fixing betty warning

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptr = (char *)malloc(size * sizeof(char));
-	if (ptr == NULL)
+	if (size == 0 || ptr == NULL)
 		return (NULL);
-	else
-	{
-		for (i = 0; i < size; i++)
-			ptr[i] = c;
-		return (ptr);
-	}
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+
+	return (ptr);
 }

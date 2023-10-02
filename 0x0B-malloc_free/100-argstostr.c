7bdaa2a72497e7d12932bf51511a7e9773c93f93
Bpaul311 <bizimapaul777@gmail.com>
2023-10-02 14:50:11 -0700

    adding file 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * argstostr - Concatenates all the arguments of your program.
+ * @ac: The argument count.
+ * @av: The argument vector.
+ *
+ * Return: A pointer to a new string, or NULL if it fails.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int counter = 0;
+	int len = 0;
+
+	/* Calculate the total length */
+	for (int i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]) + 1; /* +1 for the newline character */
+	}
+
+	/* Allocate memory for the new string */
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	/* Concatenation */
+	for (int i = 0; i < ac; i++)
+	{
+		strcpy(ptr + counter, av[i]);
+		counter += strlen(av[i]);
+		ptr[counter++] = '\n'; /* Add newline character */
+	}
+	ptr[len] = '\0'; /* Null-terminate the string */
+
+	return (ptr);
+}
+

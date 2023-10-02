34c9c486dd577453c50e335e94be411c7f5b4bd2
Bpaul311 <bizimapaul777@gmail.com>
2023-10-02 10:34:30 -0700

    fixing betty warning

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,13 @@
 #include"main.h"
 #include<stdlib.h>
 /*
- * create_array - Creates an array of characters initialized with a specific character.
+ * create_array -Creates an array of characters initialized with a character
  *
  * @size: The size of the array to create.
  * @c: The character to initialize the array with.
  *
- * This function dynamically allocates memory for an array of characters of the given size
- * and initializes each element with the specified character 'c'. Returns a pointer to the
+ * This function dynamically allocates memory for an array of characters
+ * and initializes each element with the character 'c',Returns a pointer to the
  * created array or NULL if memory allocation fails or if size is 0.
  */
 char *create_array(unsigned int size, char c)

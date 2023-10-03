ff4b9da1323dfee73bab7b122aeb4ffbc9c6a453
Bpaul311 <bizimapaul777@gmail.com>
2023-10-03 07:31:18 -0700

    first file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the executable.
+ *
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

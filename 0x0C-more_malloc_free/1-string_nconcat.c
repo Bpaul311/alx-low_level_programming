7040b426f30bf8704f13fbdc9c760aa0aead956a
Bpaul311 <bizimapaul777@gmail.com>
2023-10-05 10:55:09 -0700

    fixinf errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
  * string_nconcat - concatenates two strings up to n bytes of s2
  * @s1: the first string
@@ -10,31 +10,38 @@
  * Return: a pointer to the newly allocated memory containing the string
  *         or NULL if memory allocation fails
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned char *result;
-	unsigned int len1 = 0, len2 = 0;
-	unsigned int len3 = n
+	char *finalstr;
+	unsigned int len1, len2, i, j;
+
+	len1 = len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
 	if (n >= len2)
-		len3 = len2;
+		n = len2;
 
-	result = malloc(len1 + len3 + 1) * sizeof(char));
+	finalstr = malloc(len1 + n + 1);
 
-	if (result == NULL)
+	if (finalstr == NULL)
 		return (NULL);
 
-	strcpy(result, s1);
-	strncat(result, s2, len3);
+	for (i = 0; i < len1; i++)
+		finalstr[i] = s1[i];
 
-	return (result);
+	for (j = 0; j < n; j++)
+		finalstr[i + j] = s2[j];
+
+	finalstr[i + j] = '\0';
+
+	return (finalstr);
 }

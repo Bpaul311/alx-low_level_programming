1d4abb34d4e851614c01d8604efd249a67b3e91d
Bpaul311 <bizimapaul777@gmail.com>
2023-10-05 14:06:22 -0700

    plz work

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,44 +3,43 @@
 #include <string.h>
 
 /**
- * *_realloc - reallocates a memory block
- * @ptr: pointer to the previously allocated memory
- * @old_size: size of the allocated memory in bytes
- * @new_size: new size for the memory block in bytes
+ * *_realloc - reallocates memory to a new address
+ * @ptr: pointer to reallocate
+ * @old_size: old size in bytes
+ * @new_size: new size in bytes
  *
- * Return: pointer to the reallocated memory block
+ * Return: pointer to the newly allocated memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
-	char *dst, *src;
-	unsigned int i;
+	int min_size;
 
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-			return (NULL);
-		return (new_ptr);
-	}
+	if (new_size == old_size)
+		return (ptr);
 
-	if (new_size == 0)
+	if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (old_size < new_size)
+		min_size = old_size;
+	else if (old_size > new_size)
+		min_size = new_size;
+
 	new_ptr = malloc(new_size);
+
 	if (new_ptr == NULL)
 		return (NULL);
 
-	dst = (char *)new_ptr;
-	src = (char *)ptr;
-
-	for (i = 0; i < old_size && i < new_size; i++)
-		dst[i] = src[i];
+	memcpy(new_ptr, ptr, min_size);
 
 	free(ptr);
-
 	return (new_ptr);
 }
+

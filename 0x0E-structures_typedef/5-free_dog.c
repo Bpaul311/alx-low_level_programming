3df75cae99645b68b0e6783fe27a02fdd93d3184
Bpaul311 <bizimapaul777@gmail.com>
2023-10-09 13:40:13 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * printdog - Display dog information.
+ *
+ * @d: Pointer to the dog's data.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}

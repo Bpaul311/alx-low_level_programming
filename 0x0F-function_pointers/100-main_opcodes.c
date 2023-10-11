71909115c30a62a5b69e4b777aab5ca4bcd48ef9
Bpaul311 <bizimapaul777@gmail.com>
2023-10-11 08:58:58 -0700

    adding the last one

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - prints its own opcodes
+ * Description: Prints opcodes of its main function.
+ * Usage: ./main number_of_bytes
+ * Arguments: argc (int), argv (char *[])
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Exit status code (int)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num_bytes, i;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < num_bytes; i++)
+	{
+		if (i == num_bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
+

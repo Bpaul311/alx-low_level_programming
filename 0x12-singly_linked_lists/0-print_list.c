4b2e240cbf7498917152e470a35a35698bd71180
Bpaul311 <bizimapaul777@gmail.com>
2023-10-19 04:07:34 -0700

    corrected vetty erros

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - Prints a linked list and counts its nodes.
+ * print_list - Print and count singly linked list nodes.
  * @h: A pointer to the head of the list.
  *
- * Returns: The number of nodes in the list.
+ * Return: The number of nodes in the list. If 'h' is NULL, returns 0.
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
 	if (h == NULL)
-		return(0);
+		return (0);
 	while (h != NULL)
 	{
-		if (h -> str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		else 
-			printf("[%d] %s\n", h -> len, h -> str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
 	}
-	return(count);
+	return (count);
 }
 

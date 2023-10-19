78b7b5945897d0329fa6cde6e61480c81b36073b
Bpaul311 <bizimapaul777@gmail.com>
2023-10-19 03:23:54 -0700

    first commit

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * strtow - Splits a string into an array of words.
+ * @str: The input string to be split.
+ *
+ * Return: Pointer to an array of words (strings) or NULL if str is NULL or empty.
+ */
+char **strtow(char *str);
+

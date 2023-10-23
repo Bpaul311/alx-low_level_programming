2b75a331ec4d5b25b45d9a21f9c7c28bb1518336
Bpaul311 <bizimapaul777.com>
2023-10-23 15:08:13 -0700

    last commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,31 +14,35 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp;
+	listint_t *temp = *head;
 	listint_t *new_node;
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node == NULL)
+	if (new_node == NULL || head == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	temp = *head;
+	new_node->next = NULL;
 
-	while (i != idx && temp != NULL)
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (i < idx - 1 && temp != NULL)
 	{
 		i++;
 		temp = temp->next;
 	}
 
-	if (i == idx && temp != NULL)
+	if (i == idx - 1 && temp != NULL)
 	{
-		new_node->next = temp;
-		if (idx == 0)
-			*head = new_node;
-		else
-			temp->next = new_node;
+		new_node->next = temp->next;
+		temp->next = new_node;
 		return (new_node);
 	}
 	else

125499b21dd3d67ea8803e58af864cc21e1674ae
Bpaul311 <bizimapaul777.com>
2023-10-24 14:26:48 -0700

    adding the line that stores the next of current

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,7 +5,7 @@
  *
  * Reverses the given list in-place without using more than one loop,
  * malloc, free, or arrays.
- * Return : a pointer to the first node of the reversed list.
+ * Return: a pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
@@ -14,6 +14,7 @@ listint_t *reverse_listint(listint_t **head)
 
 	while (current != NULL)
 	{
+		next = current->next;
 		current->next = prev;
 		prev = current;
 		current = current->next;

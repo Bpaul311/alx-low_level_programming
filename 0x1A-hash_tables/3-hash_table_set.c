75fa3cf62d2dc9a6704f921c46b305af2c90d5f5
Bpaul311 <bizimapaul777.com>
2023-12-22 12:19:24 -0800

    fixinf errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,29 +9,34 @@
  * Return: Upon failure - 0.
  *         Otherwise - 1.
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
+	char *value_copy;
 	unsigned long int idx, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
 	idx = key_index((const unsigned char *)key, ht->size);
-	for (i = 0; ht->array[i]; i++)
+	for (i = idx; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(value);
-			if (ht->array[i]->value == NULL)
-				return (0);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
+
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
+		free(value_copy);
 		return (0);
 	}
 	new->key = strdup(key);
@@ -40,15 +45,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
+	new->value = value_copy;
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
+
 	return (1);
 }
-
